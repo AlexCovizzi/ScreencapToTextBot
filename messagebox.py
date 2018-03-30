87aa15fe6e6e986b858880af03391919d9af5d24
Alex Covizzi <alexcovizzi@gmail.com>
2018-03-30 17:39:34 +0200

    reorganized

--- messagebox.py
+++ messagebox.py
@@ -7,34 +7,51 @@ from PIL import Image
 
 def find(pil_img):
     cv_img = np.array(pil_img, dtype=np.uint8)
-    cv_img = preprocess(cv_img)
-    boxes = find_bounding_boxes(cv_img)
-    # sort by y
-    boxes.sort(key=lambda y: y["y1"])
+    cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
+
+    prep_img = preprocess(cv_img)
+    boxes = find_bounding_boxes(prep_img)
+    # sort by y1
+    boxes.sort(key=lambda box: box[1])
     
     return boxes
 
 def preprocess(cv_img):
     # preprocess
-    cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2GRAY)
-    cv_img = cv2.GaussianBlur(cv_img, (7, 7), 0)
-    cv_img = contrast(cv_img, 7, -210)
-    cv_img = cv2.Canny(cv_img, 40, 60, apertureSize=5)
+    cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+    cv_img = cv2.GaussianBlur(cv_img, (7,7), 0)
+    #cv_img = contrast(cv_img, 7, -210)
+    cv_img = cv2.Canny(cv_img, 20, 40)
 
     return cv_img
 
 def find_bounding_boxes(cv_img):
     boxes = []
-    _,contours,_ = cv2.findContours(cv_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        # sometimes the message box contour is open
-        cnt = cv2.convexHull(cnt)
+    _,contours,_ = cv2.findContours(cv_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # close every contour
+    for i, cnt in enumerate(contours):
+        contours[i] = cv2.convexHull(cnt)
 
-        x, y, w, h = cv2.boundingRect(cnt)
+    # build contours hierarchy i.e find outer contours
+    # we need this to keep only the contours that have at least
+    # one contour inside (probably the contour of text)
+    bounding_rects = [cv2.boundingRect(cnt) for cnt in contours]
+    hierarchy = build_hierarchy(bounding_rects)
+    
+    boxes = []
+    for member in hierarchy:
+        parent_idx = member[0]
+        children_idx = member[1]
+        cnt = contours[parent_idx]
+        x, y, w, h = bounding_rects[parent_idx]
         bounding_rect = np.array([[x, y], [x+w, y], [x+w, y+h], [x, y+h]])
-        if is_contour_mostly_rectangular(cnt, bounding_rect) and w > 40 and h > 40:
-            box = {"x1":x, "y1":y, "x2":x+w, "y2":y+h}
+        if w > 28 and h > 28 and is_contour_mostly_rectangular(cnt, bounding_rect):
+            #cv2.drawContours(cv_img, [contours[parent]], 0, (0,255,0), 3)
+            box = (x, y, x+w, y+h)
             boxes.append(box)
+
+    # TODO: remove duplicates
+    boxes = list(set(boxes))
 
     return boxes
 
@@ -46,16 +63,34 @@ def is_contour_mostly_rectangular(cnt, bounding_rect):
     last_x, last_y = cnt[0][0]
     for pt in cnt[1:]:
         x, y = pt[0]
-        if x == last_x:
+        if last_x-4 < x < last_x+4:
             v = abs(last_y-y)
             if v > vertical: vertical = v
-        if y == last_y:
+        if last_y-2 < y < last_y+2:
             h = abs(last_x-x)
             if h > horizontal: horizontal = h
         last_x = x
         last_y = y
 
-    return (ret < 0.2 and vertical > 8 and horizontal > 24)
+    return (ret < 0.15 and vertical > 8 and horizontal > 24)
+
+def build_hierarchy(rects):
+    hierarchy = []
+    for i, rect_a in enumerate(rects):
+        member = (i, [])
+        for j, rect_b in enumerate(rects):
+            if is_b_inside_a(rect_a, rect_b):
+                member[1].append(j)
+                [hierarchy.remove(m) for m in hierarchy if m[0] == j]
+                    
+        if member[1]: hierarchy.append(member)
+
+    return hierarchy
+
+def is_b_inside_a(a, b):
+    xa, ya, wa, ha = a
+    xb, yb, wb, hb = b
+    return (xa < xb < xb+wb < xa+wa and ya < yb < yb+hb < ya+ha)
 
 def contrast(cv_img, alpha=1.5, beta=-60.0):
     array_alpha = np.array([float(alpha)])
@@ -68,16 +103,19 @@ def contrast(cv_img, alpha=1.5, beta=-60.0):
 
 if __name__ == '__main__':
     import downloadimage
+    import pytesseract
 
-    urls = ["https://i.redd.it/cp5rwyl8fco01.jpg"]
+    urls = ["https://i.imgur.com/tQIXpmF.jpg","https://i.imgur.com/ub1JcdD.jpg","https://i.imgur.com/nsuVc9I.jpg","https://i.redd.it/75hvw31dg5o01.jpg"]
+    
     for url in urls:
         pil_img = downloadimage.get(url)
 
         cv_img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
-        
+
         boxes = find(pil_img)
         for box in boxes:
-            cv2.rectangle(cv_img, (box["x1"], box["y1"]), (box["x2"], box["y2"]), (0, 0, 255), 3)
-
+            bounding_rect = np.array([[box['x1'], box['y1']], [box['x2'], box['y1']], [box['x2'], box['y2']], [box['x1'], box['y2']]])
+            cv2.drawContours(cv_img, [bounding_rect], 0, (0,255,0), 3)
+            
         cv2.imshow('img', cv2.resize(cv_img, (0,0), fx=0.5, fy=0.5))
         cv2.waitKey(0)
--- ocr.py
+++ ocr.py
@@ -5,16 +5,15 @@ import pytesseract
 from statistics import mode
 import time
 import downloadimage
-
-TESSERACT_CONFIG = "-c language_model_penalty_non_dict_word=0.5 language_model_penalty_font=0.3 language_model_penalty_case=0.5 tessedit_preserve_min_wd_len=0 language_model_penalty_non_freq_dict_word=0.4 language_model_penalty_chartype=1.2"
+import messagebox
 
 # img PIL Image
 # bounds (X1, Y1, X2, Y2)
-def analyze(pil_img, bounds = None):
+def process(pil_img, bounds = None):
     if bounds: pil_img = pil_img.crop(bounds)
-        
+    
     prep_img = preprocess(pil_img)
-    string = pytesseract.image_to_string(pil_img, lang="eng")
+    string = pytesseract.image_to_string(prep_img, lang="eng", config='-psm 6 tessconfig')
     prep_img.close()
 
     return string
@@ -22,24 +21,10 @@ def analyze(pil_img, bounds = None):
 def preprocess(pil_img):
     cv_img = np.array(pil_img, dtype=np.uint8)
     cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2GRAY)
+    bordersize=10
+    cv_img = cv2.copyMakeBorder(cv_img,top=bordersize,bottom=bordersize,left=bordersize,right=bordersize,borderType=cv2.BORDER_CONSTANT,value=255 )
     cv_img = contrast(cv_img)
     return Image.fromarray(cv_img)
-
-def tesseract_ocr(pil_img):
-    string_boxes = pytesseract.image_to_boxes(pil_img, lang="eng")
-    rows = string_boxes.split("\n")
-
-    hsize = pil_img.size[1]
-    chars = []
-    for row in rows:
-        tokens = row.split(" ")
-        char = {'text':tokens[0], 'x1':int(tokens[1]), 'y1':hsize-int(tokens[4]), 'x2':int(tokens[3]), 'y2':hsize-int(tokens[2])}
-        chars.append(char)
-
-    words = chars_to_words(chars)
-    lines = words_to_lines(words)
-
-    return lines
 
 def contrast(cv_img, alpha=1.5, beta=-60.0):
     array_alpha = np.array([float(alpha)])
@@ -50,113 +35,20 @@ def contrast(cv_img, alpha=1.5, beta=-60.0):
     
     return cv_img
 
-def chars_to_words(chars):
-    words = [chars[0]]
-    for char in chars[1:]:
-        last = words[-1]
-        if last['x2']-2 < char['x1'] < last['x2']+6 and ( last['y1']-8 < char['y1'] < last['y1']+8 or last['y2']-8 < char['y2'] < last['y2']+8 ):
-            last['text'] += char['text']
-            last['x2'] = char['x2']
-            if char['y1'] < last['y1']: last['y1'] = char['y1']
-            if char['y2'] > last['y2']: last['y2'] = char['y2']
-        else:
-            words.append(char)
-
-    return words
-
-def words_to_lines(words):
-    lines = [words[0]]
-    for word in words[1:]:
-        last = lines[-1]
-        if last['x2']-2 < word['x1'] < last['x2']+16 and ( last['y1']-6 < word['y1'] < last['y1']+6 or last['y2']-6 < word['y2'] < last['y2']+6 ):
-            last['text'] += " "+word['text']
-            last['x2'] = word['x2']
-        else:
-            lines.append(word)
-
-    return lines
-
-def find_squares(img):
-    img = cv2.GaussianBlur(img, (5, 5), 0)
-    squares = []
-    for gray in cv2.split(img):
-        for thrs in range(0, 255, 26):
-            if thrs == 0:
-                bin = cv2.Canny(gray, 0, 50, apertureSize=5)
-                bin = cv2.dilate(bin, None)
-            else:
-                _retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
-            bin, contours, _hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            for cnt in contours:
-                cnt_len = cv2.arcLength(cnt, True)
-                cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
-                if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
-                    cnt = cnt.reshape(-1, 2)
-                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
-                    if max_cos < 0.1:
-                        squares.append(cnt)
-    return squares
-
-def angle_cos(p0, p1, p2):
-    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
-    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
-
-# devono esserci almeno 1 linea orizzontale e una verticale lunga un tot
-def is_contour_message_box(cnt):
-    cnt_area = cv2.contourArea(cnt)
-    if cnt_area < 1600:
-        return False
-    cnt_len = cv2.arcLength(cnt, False)
-    #cnt = cv2.approxPolyDP(cnt, 0.001*cnt_len, False)
-    vertical = 0
-    horizontal = 0
-    last = cnt[0]
-    for point in cnt[1:]:
-        x = point[0][0]
-        y = point[0][1]
-        last_x = last[0][0]
-        last_y = last[0][1]
-        if x == last_x:
-            v = abs(last_y-y)
-            if v > vertical: vertical = v
-        if y == last_y:
-            h = abs(last_x-x)
-            if h > horizontal: horizontal = h
-        last = point
-    #print("Vertical: {}, Horizontal: {}".format(vertical, horizontal))
-    if vertical > 12 and horizontal > 16:
-        return True
-    else:
-        return False
-
 if __name__ == '__main__':
-    url = "https://i.redd.it/lx032y3l60n01.jpg"
+    url = "https://i.redd.it/75hvw31dg5o01.jpg"
     pil_img = downloadimage.get(url)
-
-    #lines = analyze(pil_img)
 
     cv_img = np.array(pil_img, dtype=np.uint8)
     cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
-    gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.medianBlur(gray, 7)
-    gray = contrast(gray, 7.5, -220)
-    #cv2.imshow('img', cv2.resize(gray, (0,0), fx=0.4, fy=0.4))
-    #cv2.waitKey(0)
-    #squares = find_squares(cv_img)
-    #cv2.drawContours( cv_img, squares, -1, (0, 255, 0), 3 )
 
-    gray = cv2.Canny(gray, 40, 60, apertureSize=5)
-    #cv2.imshow('img', cv2.resize(gray, (0,0), fx=0.4, fy=0.4))
-    #cv2.waitKey(0)
-
-    _,contours,_ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(cv_img,contours,-1,(0,0,255),1)
-
-    for cnt in contours:
-        if is_contour_message_box(cnt):
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.drawContours(cv_img,[cnt],0,(0,255,0),3)
-            
+    boxes = messagebox.find(pil_img)
+    for box in boxes:
+        x1, y1, x2, y2 = box
+        bounding_rect = np.array([[x1, y1], [x2, y1], [x2, y2], [x1, y2]])
+        cv2.drawContours(cv_img, [bounding_rect], 0, (0,255,0), 3)
+        text = process(pil_img, (x1, y1, x2, y2))
+        print(text)
 
     cv2.imshow('img', cv2.resize(cv_img, (0,0), fx=0.5, fy=0.5))
     cv2.waitKey(0)
